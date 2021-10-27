d6853ba3db00ce5af81175d162d1b45e2e3daf6f
herwan <mdherwan@gmail.com>
2021-10-27 13:03:21 +0800

    factory bot intro

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'test a number to be a positive' do
-    expect(1).to be_positive # be_positive => 1.posotive? == true
-    expect(5).to be > 2
+  # it 'test a number to be a positive' do
+    # expect(1).to be_positive # be_positive => 1.posotive? == true
+    # expect(5).to be > 2
+  it 'tests article object' do
+    article = create(:article)
+    expect(article.title).to eq('Sample article')
   end
 end
