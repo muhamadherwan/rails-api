10c3ec39f99edf9eec12fffff9be2f3ddd809a8d
herwan <mdherwan@gmail.com>
2021-10-29 16:21:54 +0800

    assgmnt answer

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,11 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  # it 'test a number to be a positive' do
-    # expect(1).to be_positive # be_positive => 1.posotive? == true
-    # expect(5).to be > 2
-  it 'tests article object' do
-    article = create(:article)
-    expect(article.title).to eq('Sample article')
+  describe '#validations' do
+    let(:article) { build(:article) }
+
+    it 'tests that factory is valid' do
+      expect(article).to be_valid # article.valid ? == true
+      article.save!
+      another_article = build(:article)
+      expect(another_article).to be_valid
+    end
+
+    it 'has an invalid title' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
+
+    it 'has an invalid content' do
+      article.content = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:content]).to include("can't be blank")
+    end
+
+    it 'has an invalid slug' do
+      article.slug = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:slug]).to include("can't be blank")
+    end
+
+    it 'validates the uniqueness of the slug' do
+      article1 = create(:article)
+      expect(article1).to be_valid
+      article2 = build(:article, slug: article1.slug)
+      expect(article2).not_to be_valid
+      expect(article2.errors[:slug]).to include('has already been taken')
+    end
+
   end
 end
