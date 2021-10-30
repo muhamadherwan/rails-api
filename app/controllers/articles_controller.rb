7cefc2c636c3ac32acb7bec748988d001e65099b
herwan <mdherwan@gmail.com>
2021-10-30 17:43:57 +0800

    create article show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,9 @@ class ArticlesController < ApplicationController
   def index
     render json:{'data':'ayam'}
   end
+
+  def show
+    article = Article.find(params[:id])
+    render json:{data:article}
+  end
 end
