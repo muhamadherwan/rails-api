512f9a4ee280bcc000d6fc201007319ff18c36ed
herwan <mdherwan@gmail.com>
2021-11-13 18:14:38 +0800

    create session for success login

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -2,5 +2,8 @@ class AccessTokensController < ApplicationController
   def create
     authenticator = UserAuthenticator.new(params[:code])
     authenticator.perform
+    
+    render json: aunthenticator.access_token, status: :created
+
   end
 end
