0b38cb11f9bfef8e0f93ab0a8201cae717f81f97
herwan <mdherwan@gmail.com>
2021-11-13 17:37:25 +0800

    login user endpoint - unauthorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::API
-  include JsonapiErrorsHandler
+  #include JsonapiErrorsHandler
   
-  ErrorMapper.map_errors!({
-      'ActiveRecord::RecordNotFound' => 
+  #ErrorMapper.map_errors!({
+  #volkan    'ActiveRecord::RecordNotFound' => 
       'JsonapiErrorsHandler::Errors::NotFound'
-  })
-  rescue_from ::StandardError, with: lambda { |e| handle_error(e) }
-  
+  #)
+  #escue_from ::StandardError, with: lambda { |e| handle_error(e) }
+  rescue_from UserAuthentication::AuthenticationError, with: :authentication_error
+
+  private
+
+ def authentication_error
+   error = {
+     "status" => "401",
+     "source" => { "pointer" => "/code" },
+     "title" => "Authentication code is invalid"
+     "detail" => "You must provide valid code in order to exchange it for a token."
+   }
+   render json: { "errors": [ error ]}, status: 401
+ end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   #get '/articles', to: 'articles#index'
+  post 'login', to: 'access_token#create'
   resources :articles, only: [:index, :show]
 end
