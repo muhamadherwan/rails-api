fd2c8c297bfb66bb60d0b6d006ac2bfc2be0c316
herwan <mdherwan@gmail.com>
2021-11-24 02:16:40 +0800

    destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,6 +38,15 @@ class ArticlesController < ApplicationController
       status: :unprocessable_entity
   end
 
+  def destroy
+    article = current_user.articles.find(params[:id])
+    article.destroy
+    head :no_content
+    # if fails
+  rescue
+    authorization_error
+  end
+
   def serializer
     ArticleSerializer
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   #get '/articles', to: 'articles#index'
   post 'login', to: 'access_token#create'
   delete 'logout', to: 'access_token#destroy'
-  resources :articles, only: [:index, :show, :create, :update]
+  #resources :articles, only: [:index, :show, :create, :update, :destroy]
+  resources :articles
 end
